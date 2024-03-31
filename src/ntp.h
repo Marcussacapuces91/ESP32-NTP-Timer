f665f931d0bd239ae73ccbf81716d311496015d9
Marc Sibert <marc@sibert.fr>
2024-04-01 00:28:33 +0200

    Bug de setT0
    oublié le modulo des micros

--- src/ntp.h
+++ src/ntp.h
@@ -18,8 +18,9 @@
 
 #include <cstdint>
 #include <cstring>
+#include <arduino.h>
 
-#define byte unsigned char
+// #define byte unsigned char
 #define YEAR1970 2208988800
 
 class NTP {
@@ -41,6 +42,10 @@ class NTP {
  */
     static byte packetSize();
 
+/**
+ * Retourne un chaîne de caractères décrivant l'enregistrement Header du paquet NTP.
+ * @return un pointeur sur la chaîne.
+ */
     const char* getHeader() const;
     byte getMode() const;
     byte getVersion() const;
@@ -52,7 +57,17 @@ class NTP {
     uint64_t getT1() const;
     uint64_t getT2() const;
     uint64_t getT3() const;
+
+/**
+ * Retourne l'offset en microsecondes.
+ * @return Ecart signé en microsecondes.
+ */
     int64_t getOffset() const;
+
+/**
+ * Round-trip delay time.
+ */
+    unsigned long getRTT() const;
     void setPacket(const uint8_t buffer[]);
     void setT0(const uint64_t& tx);
     void setT3(const uint64_t& rx);
@@ -82,7 +97,7 @@ class NTP {
       uint8_t txTm_s[4];      // 32 bits and the most important field the client cares about. Transmit time-stamp seconds.
       uint8_t txTm_f[4];      // 32 bits. Transmit time-stamp fraction of a second.
 
-    } packet;              // Total: 384 bits or 48 bytes.
+    } packet;                 // Total: 384 bits or 48 bytes.
 
     uint64_t t3;
 
@@ -97,24 +112,30 @@ inline void NTP::setPacket(const uint8_t buffer[]) {
   memcpy(&packet, buffer, sizeof(ntp_packet));
 }
 
+/**
+ * Set Transmit Timestamp before send.
+ * @param tx Transmit Timestamp in µsec since 1/1/1900.
+ * @warning T0 must be write in Transmit Timestamp before sending to server.
+ */
 inline void NTP::setT0(const uint64_t& tx) {
-  uint32_t sec = (tx / 1000000);
+  const uint32_t d = tx / 1000000;      // div
+  const uint64_t m = tx - d * 1000000;  // mod
+
+  uint32_t sec = d;
   packet.txTm_s[3] = sec & 0xFF;
   sec >>= 8;
   packet.txTm_s[2] = sec & 0xFF;
   sec >>= 8;
   packet.txTm_s[1] = sec & 0xFF;
-  sec >>= 8;
-  packet.txTm_s[0] = sec & 0xFF;
+  packet.txTm_s[0] = sec >> 8;
 
-  uint32_t micros = (tx << 32) / 1000000;
+  uint32_t micros = (m << 32) / 1000000;
   packet.txTm_f[3] = micros & 0xFF;
   micros >>= 8;
   packet.txTm_f[2] = micros & 0xFF;
   micros >>= 8;
   packet.txTm_f[1] = micros & 0xFF;
-  micros >>= 8;
-  packet.txTm_f[0] = micros & 0xFF;
+  packet.txTm_f[0] = micros >> 8;
 }
 
 inline void NTP::setT3(const uint64_t& rx) {
