778e3204edac8a46262b18d6ecaa193ab04b4140
Marc Sibert <marc@sibert.fr>
2024-04-11 09:16:09 +0200

    Update src.ino

--- src/src.ino
+++ src/src.ino
@@ -14,17 +14,14 @@
 //    limitations under the License.
 //
 
-// #define WIFI_SSID
-// #define WIFI_PASS
-
-#include "secrets.h"
 #include "application.h"
-
 Application app;
 
 void setup() {
   Serial.begin(115200);
-  delay(500);
+  while (!Serial) yield();
+  Serial.println("ESP32 NTP Timer V0");
+  Serial.println("Copyright (c) 2024 M. Sibert");
   app.setup();
 }
 
